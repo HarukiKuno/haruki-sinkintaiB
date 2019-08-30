79946dd7505b2596f962eb7ad9f5137cff309d24
haruki123 <serasu123@gmail.com>
2019-08-30 12:35:31 +0000

    Finish login-function

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,27 @@
 Rails.application.routes.draw do
   
 
+  get 'sessions/new'
+
   root 'static_pages#top'
+  
+  
   get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  
+  
+  
+  #ログイン機能
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
+  
+  
+  
+  
   
   resources :users
 end

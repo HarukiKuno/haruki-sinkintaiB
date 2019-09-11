60152a8e0336ffd232042d9edd66b1f0aee8cc79
haruki123 <serasu123@gmail.com>
2019-09-11 15:41:09 +0000

    Finish add-basic-info

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,17 @@
 Rails.application.routes.draw do
   
-
-  get 'sessions/new'
-
   root 'static_pages#top'
-  
-  
   get '/signup', to: 'users#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  
-  
-  
-  #ログイン機能
-  
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
+
+  # ログイン機能
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  
-  
-  
-  
-  
-  
-  resources :users
+
+  resources :users do
+    member do
+      get 'edit_basic_info'
+      patch 'update_basic_info'
+    end
+  end
 end
